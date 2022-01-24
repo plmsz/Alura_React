b040e29a2b613f009036df861592a34b0a2c86de
Paloma <85586862+plmsz@users.noreply.github.com>
2022-01-24 00:10:36 -0300

    js_oo

--- 01_Orientação a Objetos/Projeto/ContaCorrente.js
+++ 01_Orientação a Objetos/Projeto/ContaCorrente.js
@@ -1,21 +1,43 @@
-export class ContaCorrente{
-    agencia;
-     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
+import { Cliente } from "./Cliente.js";
 
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
+export class ContaCorrente {
+  agencia;
+  _cliente;
+  // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+  _saldo = 0; //convenções atuais, ele não deve ser alterado fora da classe, mas o JS não impede de usa-lo
 
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
-        this._saldo += valor;           
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
     }
+  }
+
+  get cliente() {
+    return this._cliente;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+
+  sacar(valor) {
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+      return valor;
+    }
+  }
+
+  depositar(valor) {
+    if (valor <= 0) {
+      return;
+    }
+    this._saldo += valor;
+  }
+  //composição de classes (conta é um objeto da classe ContaCorrente)
+  transferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+    valor = 20; //parametro valor é uma cópia, logo valor no index não será alterado
+    // conta.cidade = "Salvador" CUIDADO, irá alterá, pois conta é um tipo de referência
+  }
 }
-
--- 01_Orientação a Objetos/Projeto/index.js
+++ 01_Orientação a Objetos/Projeto/index.js
@@ -1,5 +1,5 @@
-import { Cliente } from './Cliente.js';
-import { ContaCorrente } from './ContaCorrente.js';
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -9,16 +9,24 @@ const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
-
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+// contaCorrenteRicardo.cliente = 0; //retorna cliente undefined
+contaCorrenteRicardo.cliente = cliente1;
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.agencia = 1001;
+conta2.cliente = cliente2;
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+// contaCorrenteRicardo.depositar(500);
+// let valor = 200; //tipo primitivo
+// contaCorrenteRicardo.transferir(valor, conta2)
+// console.log("valor:", valor)
 
 console.log(contaCorrenteRicardo);
+console.log(conta2.cliente);
 
+// conta2.saldo = 3000;
+// console.log(conta2.saldo); //só tem getter não consegue atribuir um valor
+
+console.log(conta2); 
