bb23c121023f49948cdaaad6e0eb5c26efeb0934
paloma.souza <paloma.souza@ambevtech.com.br>
2022-02-21 20:17:52 -0300

    react hooks

--- Formacao_React/06_Hooks_Context/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ Formacao_React/06_Hooks_Context/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,23 +2,27 @@ import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import { useEffect, useState } from "react";
+import { Typography } from "@material-ui/core";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDadosColetados] = useState({});
 
-  useEffect(()=>{
-    console.log(dadosColetados)
-  })
+  useEffect(() => {
+    if (etapaAtual === formularios.length - 1) {
+      aoEnviar(dadosColetados);
+    }
+  });
 
   const formularios = [
     <DadosUsuario aoEnviar={coletarDados} />,
     <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
     <DadosEntrega aoEnviar={coletarDados} />,
+    <Typography variant="h5">Obrigado por se cadastrar!</Typography>,
   ];
 
   function coletarDados(dados) {
-    setDadosColetados({...dadosColetados,...dados})
+    setDadosColetados({ ...dadosColetados, ...dados });
     proximo();
   }
 
