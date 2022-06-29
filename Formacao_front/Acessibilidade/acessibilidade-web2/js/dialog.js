b7be73e1dcf2aff26568668e5d9bdb1368fb9ae1
paloma.souza <paloma.souza@ambevtech.com.br>
2022-06-29 18:02:24 -0300

    Modal acessivel, titulos, alt

--- Formacao_front/Acessibilidade/acessibilidade-web2/js/dialog.js
+++ Formacao_front/Acessibilidade/acessibilidade-web2/js/dialog.js
@@ -1,19 +1,35 @@
 // Variáveis
 var btnAbreDialog = document.querySelector('#abreDialog');
-var dialog = document.querySelector('.dialogNewsletter');
+var dialog = document.querySelector('.dialogNewsletter'); 
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
-
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+btnAbreDialog.style.display = 'block';
 
 // Quando abrir a dialog...
-btnAbreDialog.addEventListener('click', function() {
+btnAbreDialog.addEventListener('click', function () {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.inert = true;
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');
+  conteudoForaDialog.inert = false;
+  btnAbreDialog.focus();
+
 }
 
 // Listeners
+
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+
+dialogOverlay.addEventListener('click', fechandoDialog);
+
+document.addEventListener('keyup', function (event) {
+  console.log(event.key);
+  if (event.key === 'Escape') {
+    fechandoDialog();
+  }
+});
